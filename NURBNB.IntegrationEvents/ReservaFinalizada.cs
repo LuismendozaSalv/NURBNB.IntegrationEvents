83adae44735bd1d6d0b934995e77b4bd66413bd0
Luis Mendoza <lumendoza@bncr.fi.cr>
2023-11-07 15:29:28 -0400

    Add project files.

---
+++ NURBNB.IntegrationEvents/ReservaFinalizada.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NURBNB.IntegrationEvents
+{
+    public class ReservaFinalizada
+    {
+        public Guid PropiedadId { get; set; }
+        public Guid ReservaId { get; set; }
+    }
+}
