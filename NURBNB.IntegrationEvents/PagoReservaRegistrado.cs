ea8e60b557ddeccd3de7039a116ce103ccde07e9
Luis Mendoza <luismendozasalv@gmail.com>
2024-01-26 23:09:56 -0400

    New events

---
+++ NURBNB.IntegrationEvents/PagoReservaRegistrado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NURBNB.IntegrationEvents
+{
+	public class PagoReservaRegistrado
+	{
+		public Guid ReservaId { get; set; }
+		public Guid TransaccionId { get; set; }
+	}
+}
---
+++ NURBNB.IntegrationEvents/ReservaAceptada.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NURBNB.IntegrationEvents
+{
+	public class ReservaAceptada
+	{
+
+		public Guid GuestId { get; set; }
+		public Guid reservaId { get; set; }
+		public Guid PropiedadId { get; set; }
+		public decimal Monto { get; set; }
+	}
+}
